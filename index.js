afe00d0dac3de0794d21589860aa6f5758787fdb
Laura Bodell <bodelllaura@gmail.com>
2021-06-28 16:33:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 
 const removeFirst = ["Milo", "Otis", "Garfield"]
+
+
+
+append.push("Odie")
+prepend.unshift("Odie")
+removeLast.pop()
+removeFirst
